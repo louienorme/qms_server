ec54d6f992090338279ac9e8963fb6fef7532c18
John Loui Enorme <48955345+FreshMaker-L@users.noreply.github.com>
2021-09-23 20:57:27 +0800

    User Schema

---
+++ src/accounts/user/model.ts
@@ -0,0 +1,30 @@
+import mongoose, { Schema } from 'mongoose';
+import IUser from './_interface';
+
+const UserSchema: Schema = new Schema(
+    {
+        userId: {
+            type: String
+        },
+        fullName: {
+            firstName: String,
+            middleName: String,
+            lastName: String,
+            nameExtension : String
+        },
+        contact: {
+            email: String
+        },
+        username: {
+            type: String
+        },
+        password: {
+            type: String
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+export default mongoose.model<IUser>('User', UserSchema);
